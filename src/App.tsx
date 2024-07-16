cb0c5ff6f6345d43778975429e49b8989d9e5ea0
Afimc <afimc1912@gmail.com>
2024-07-16 17:59:52 +0100

    basic structure created. main landing page done with figma designe

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <div className="logo">
+          <div className="left">UP</div>
+          <div className="right">
+            <div className="work">work</div>
+            <div className="date">date</div>
+          </div>
+        </div>
+        <div className='h2-wrapper'>
+          <h2>Create a Professional Upwork Profile with AI Assistance</h2>
+          <div className="line"></div>
+        </div>
+        <div className="buttons">
+          <button className='button-blue'>New Profile</button>
+          <button className='button-white'>Update</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
 export default App
+
+
